d1650bf425130606cb9c7d4b98f51e88199c6242
Heloisa Albuquerque <heloisaalbuquerque1706@gmail.com>
2025-04-07 18:23:46 -0300

    adição de métodos auxiliares na classe Cliente
    Foram adicionados os métodos Setters em todos os atributos, toString, equals e hasCode

--- src_camadas/src/negocio/entidade/Cliente.java
+++ src_camadas/src/negocio/entidade/Cliente.java
@@ -1,6 +1,7 @@
 package negocio.entidade;
 
 import excecoes.dados.ErroAoCarregarDadosException;
+import java.util.Objects;
 
 public class Cliente {
     private String cpf;
@@ -16,13 +17,33 @@ public class Cliente {
     public String getCpf() {
         return cpf;
     }
+    public void setCpf(String cpf) { this.cpf = cpf;}
 
     public String getNome() {
         return nome;
     }
+    public void setNome(String nome){this.nome = nome;}
 
     public String getEmail() {
         return email;
     }
+    public void setEmail(String email) { this.email = email;}
 
+    @Override
+    public String toString(){
+        return "Cliente(" + "cpf: '"+cpf+ '\'' + ", nome: '"+nome+'\''+", email: '"+email+'\'' + ')';
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this==o) return true;
+        if(!(o instanceof Cliente)) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(cpf, cliente.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf);
+    }
 }
