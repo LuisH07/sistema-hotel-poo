68963f6ece50384532acb6b6ea9a968c11db3755
Arthur-789 <126814057+Arthur-789@users.noreply.github.com>
2025-04-07 23:05:33 -0300

    Closes #19 - Lógica de Negócio Atendente

--- src_camadas/src/negocio/IFluxoReservas.java
+++ src_camadas/src/negocio/IFluxoReservas.java
@@ -1,15 +1,30 @@
 package negocio;
 
+import excecoes.dados.ErroAoSalvarDadosException;
+import excecoes.negocio.cliente.ClienteInvalidoException;
+import excecoes.negocio.quarto.QuartoInvalidoException;
+import excecoes.negocio.reserva.ConflitoDeDatasException;
+import excecoes.negocio.reserva.ReservaInvalidaException;
+import excecoes.negocio.reserva.ReservaNaoEncontradaException;
 import negocio.entidade.Cliente;
-import negocio.entidade.QuartoAbstrato;
+import negocio.entidade.Reserva;
 
-import java.time.LocalDate;
+import java.util.List;
 
 public interface IFluxoReservas {
 
-    default void fazerReserva(Cliente cliente, QuartoAbstrato quarto, LocalDate dataInicio,
-                              LocalDate dataFim){}
-    void cancelarReserva(String idReserva);
-    void consultarHistorico();
+    void fazerReserva(Cliente cliente, String numeroQuarto, String dataInicioString, String dataFimString)
+            throws ReservaInvalidaException, ClienteInvalidoException,
+            QuartoInvalidoException, ConflitoDeDatasException, ErroAoSalvarDadosException;
+
+    void cancelarReserva(String idReserva) throws ReservaNaoEncontradaException, ReservaInvalidaException, ErroAoSalvarDadosException;
+
+    List<Reserva> consultarHistorico();
+
+    void fazerCheckIn(String idReserva) throws ReservaNaoEncontradaException,
+            ReservaInvalidaException, ErroAoSalvarDadosException;
+
+    void fazerCheckOut(String idReserva) throws ReservaNaoEncontradaException,
+            ReservaInvalidaException, ErroAoSalvarDadosException;
 
 }
